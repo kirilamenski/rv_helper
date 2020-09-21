9f83709b94163d12872fa9e76362d73f7d540863
KirilaMenski <bv_1514@mail.ru>
2020-09-21 21:30:41 +0300

    update example

--- app/src/main/java/com/ansgar/recyclerviewdemo/ImageViewHolder.kt
+++ app/src/main/java/com/ansgar/recyclerviewdemo/ImageViewHolder.kt
@@ -2,10 +2,19 @@ package com.ansgar.recyclerviewdemo
 
 import android.view.View
 import com.ansgar.rvhelper.BaseViewHolder
+import kotlinx.android.synthetic.main.view_holder_image.view.*
 
-class ImageViewHolder(view: View) : BaseViewHolder<MainActivity.Image>(view) {
+class ImageViewHolder(private val view: View) : BaseViewHolder<MainActivity.Image>(view) {
     override fun bind(item: MainActivity.Image) {
-
+        with(view) {
+            val rnd = (0..10).random()
+            image_view_holder_iv.setImageResource(
+                when {
+                    rnd % 2 == 0 -> R.drawable.ic_launcher_background
+                    else -> R.drawable.ic_launcher_foreground
+                }
+            )
+        }
     }
 
 }
