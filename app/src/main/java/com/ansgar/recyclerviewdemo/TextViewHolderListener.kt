627e0c307369ecda3e31462f84e27ddfcb3bccb6
KirilaMenski <bv_1514@mail.ru>
2020-10-02 20:11:44 +0300

    deleted unnecessary files

--- app/src/main/java/com/ansgar/recyclerviewdemo/TextViewHolderListener.kt
+++
@@ -1,7 +0,0 @@
-package com.ansgar.recyclerviewdemo
-
-import com.ansgar.rvhelper.holders.BaseViewHolderListener
-
-interface TextViewHolderListener : BaseViewHolderListener<MainActivity.User> {
-    fun onTextClicked(user: MainActivity.User, position: Int)
-}
