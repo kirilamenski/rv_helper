ff676ee40ee96e5558219fedcb497a6ced4d7c25
KirilaMenski <bv_1514@mail.ru>
2020-10-05 18:05:32 +0300

    update example

--- app/src/main/java/com/ansgar/example/ExampleActivity.kt
+++ app/src/main/java/com/ansgar/example/ExampleActivity.kt
@@ -1,6 +1,7 @@
 package com.ansgar.example
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ansgar.example.holders.ImageViewHolder
@@ -14,19 +15,47 @@ import com.ansgar.rvhelper.adapters.MultipleTypesAdapter
 import com.ansgar.rvhelper.adapters.SingleTypeAdapter
 import com.ansgar.rvhelper.createMultipleTypesAdapter
 import com.ansgar.rvhelper.createSingleTypeAdapter
+import com.ansgar.rvhelper.holders.DefaultLoadingViewHolder
 import com.ansgar.rvhelper.models.ViewHolderItem
+import com.ansgar.rvhelper.scroll.OnPageChanged
+import com.ansgar.rvhelper.scroll.RvScrollListener
 import com.ansgar.rvhelper.viewHoldersUtil
 import com.ansgar.rvhelperexample.R
 import kotlinx.android.synthetic.main.example_activity.*
 
 class ExampleActivity : AppCompatActivity() {
 
-//    private lateinit var rvAdapter: SingleTypeAdapter<ExampleUser>
+    //    private lateinit var rvAdapter: SingleTypeAdapter<ExampleUser>
+    private lateinit var rvAdapter: MultipleTypesAdapter
+    private val viewHoldersUtil = viewHoldersUtil {
+        create(R.layout.view_holder_user) { view -> UserViewHolder(view) }
+        create(R.layout.view_holder_image) { view -> ImageViewHolder(view) }
+        create(R.layout.view_holder_text) { view -> TextViewHolder(view) }
+        createLoadingViewHolder { DefaultLoadingViewHolder(it) }
+    }
+    private val onRvScrollListener = RvScrollListener(object : OnPageChanged {
+        override fun onPageChanged(page: Int) {
+            runnable = Runnable {
+                updateList(if(page < 3) getFakeUsers(page * 20 + 1) else ArrayList())
+            }
+            handler?.postDelayed(runnable, 500)
+        }
+    })
+
+    // Handler and runnable just to simulate latency of backend response
+    private var handler: Handler? = null
+    private var runnable: Runnable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.example_activity)
         createRecyclerView()
+        handler = Handler()
+    }
+
+    override fun onDestroy() {
+        runnable?.let { handler?.removeCallbacks(it) }
+        super.onDestroy()
     }
 
     private fun createRecyclerView() {
@@ -43,7 +72,8 @@ class ExampleActivity : AppCompatActivity() {
 //            addAll(getFakeUsers())
 //        }
         rvAdapter = viewHoldersUtil.createMultipleTypesAdapter {
-            addAll(getFakeUsers())
+            addAll(getFakeUsers(1))
+            onScrollingObserver = onRvScrollListener
         }
         with(example_rv) {
             layoutManager = LinearLayoutManager(
@@ -52,19 +82,17 @@ class ExampleActivity : AppCompatActivity() {
                 false
             )
             adapter = rvAdapter
+            addOnScrollListener(onRvScrollListener)
         }
     }
 
-    private lateinit var rvAdapter: MultipleTypesAdapter
-    private val viewHoldersUtil = viewHoldersUtil {
-        create(R.layout.view_holder_user) { view -> UserViewHolder(view) }
-        create(R.layout.view_holder_image) { view -> ImageViewHolder(view) }
-        create(R.layout.view_holder_text) { view -> TextViewHolder(view) }
+    private fun updateList(fakeItems: ArrayList<ViewHolderItem>) {
+        rvAdapter.addAll(fakeItems)
     }
 
-    private fun getFakeUsers(): ArrayList<ViewHolderItem> {
+    private fun getFakeUsers(start: Int): ArrayList<ViewHolderItem> {
         val arrayList = ArrayList<ViewHolderItem>()
-        for (i in 1..50) {
+        for (i in start..(start + 19)) {
             when {
                 i % 5 == 0 -> arrayList.add(
                     ExampleImage(
