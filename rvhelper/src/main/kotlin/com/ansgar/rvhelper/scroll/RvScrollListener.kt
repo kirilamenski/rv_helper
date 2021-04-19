8e9060f6b421595a7f84e1d16d73d8c67eb31234
Kirila Menski <bv_1514@mail.ru>
2021-04-19 14:49:44 +0300

    add scrolling for horizontal orientation

--- rvhelper/src/main/kotlin/com/ansgar/rvhelper/scroll/RvScrollListener.kt
+++ rvhelper/src/main/kotlin/com/ansgar/rvhelper/scroll/RvScrollListener.kt
@@ -1,9 +1,11 @@
 package com.ansgar.rvhelper.scroll
 
 import androidx.annotation.CallSuper
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-open class RvScrollListener(private val onPageChanged: OnPageChanged) : RecyclerView.OnScrollListener(),
+open class RvScrollListener(private val onPageChanged: OnPageChanged) :
+    RecyclerView.OnScrollListener(),
     RvAdapterObserver {
 
     private var page: Int = 0
@@ -12,7 +14,14 @@ open class RvScrollListener(private val onPageChanged: OnPageChanged) : Recycler
     @CallSuper
     override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
         super.onScrollStateChanged(recyclerView, newState)
-        if (!recyclerView.canScrollVertically(1) && !stopScrolling) {
+        val orientation = (recyclerView.layoutManager as? LinearLayoutManager?)?.orientation
+            ?: LinearLayoutManager.VERTICAL
+        val canScroll = if (orientation == LinearLayoutManager.VERTICAL) {
+            !recyclerView.canScrollVertically(1)
+        } else {
+            !recyclerView.canScrollHorizontally(1)
+        }
+        if (canScroll && !stopScrolling) {
             page++
             onPageChanged.onPageChanged(page)
         }
